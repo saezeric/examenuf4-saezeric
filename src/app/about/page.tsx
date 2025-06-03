d0bd3d143ec3bd62bfefcf8b8a1714ba9f30f5ea
saezeric <saezescalonaeric@fpllefia.com>
2025-06-03 18:16:04 +0200

    Ejercicio 3 terminado

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import React from "react";
 
-export default function MovieExplorer() {
+export default function About() {
   return (
     <main>
       <div className="min-h-screen flex justify-center items-center">
         <div className="text-center">
           <h2 className="text-2xl font-bold mb-4">
-            Bienvenido a la pagina de Movie Explorer
+            Bienvenido a la pagina de About
           </h2>
         </div>
       </div>
--- src/app/misComponentes/header.tsx
+++ src/app/misComponentes/header.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="py-7 bg-black">
-      <h1 className="text-white text-center text-2xl font-semibold">
-        Eric Saez Escalona
-      </h1>
+      <nav className="flex justify-center space-x-6">
+        <h1 className="text-2xl mr-20">Eric Saez Escalona</h1>
+        <Link
+          href="/"
+          className="text-white text-lg font-semibold hover:underline"
+        >
+          Movie Explorer
+        </Link>
+        <Link href="/about" className="text-white text-lg hover:underline">
+          About
+        </Link>
+      </nav>
     </header>
   );
 }
